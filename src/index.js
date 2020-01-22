723f0265744f919151064383c34d09c502aa25dc
Paris Tyus <Tyus.Paris1@gmail.com>
2020-01-22 10:40:05 -0600

    finished exercise

---
+++ src/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const $ = require('jquery');
+
+
+let sayHello = (name) => {
+    console.log('Paris');
+};
+sayHello();
+
+$('body').css('background-color', 'beige');
+
+$('#main-container').css('background-color', 'green');
+
+
+$(".div").css('background-color', 'lightblue');
+
+
+
+
+
